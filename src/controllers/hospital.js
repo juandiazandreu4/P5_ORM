7fcc4bd8c69ec6bfe103982eab99a7a745b92e58
juandiazandreu4 <juanjrdiazandreu@gmail.com>
2024-03-29 00:17:13 +0100

    hospital create

--- src/controllers/hospital.js
+++ src/controllers/hospital.js
@@ -2,12 +2,23 @@ const {models} = require('../models');
 
 // Crear un  hospital
 exports.create = async function (name, city) {
-    // Rellene aqui ...
-};
+    try{ //para que sea asincrono
+        let hospital = await models.hospital.build({
+            name: name,
+            city: city
+        });
+        
+        hospital.save();
+
+        return hospital;
+
+    } catch (error) {
+        console.error('Error', error);
+    }};
 
 // Devuelve todos los hospitales
 exports.index = async function () {
-    // Rellene aqui ...
+    
 }
 
 // Filtra los hospitales por ciudad
