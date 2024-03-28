73c95aefc10c23d87b123e01ca549e1b9ab415f2
juandiazandreu4 <juanjrdiazandreu@gmail.com>
2024-03-29 00:28:09 +0100

    doctor indexbypatient

--- src/controllers/doctor.js
+++ src/controllers/doctor.js
@@ -1,17 +1,41 @@
 const {models} = require('../models');
+const hospital = require('../models/hospital');
 
 // Crear un doctor
 exports.create = async function (name, surname, speciality) {
-    // Rellene aqui ...
+    try{ //para que sea asincrono
+        let doctor = await models.doctor.build({
+            name: name,
+            surname: surname,
+            speciality: speciality,
+        });
+        
+        doctor.save();
+
+        return doctor;
+
+    } catch (error) {
+        console.error('Error', error);
+    }
 };
 
 
 // Asigna un doctor y devuelve los datos del paciente
 exports.assignDoctor = async function (patientId, doctorId) {
-    // Rellene aqui ...
+
+    let doctor = await models.doctor.findByPk(doctorId);
+    let patient = await models.patient.findByPk(patientId);
+    
+    patient = await patient.addDoctor(doctor); //de donde sale esa funcion?
+
+    return patient;
+
 }
 
 // Muestra los medicos de un paciente
 exports.indexByPatient = async function (patientId) {
-    // Rellene aqui ...
+    let patient = await models.patient.findByPk(patientId);
+    let doctors = await patient.getDoctors(); //de donde sale esa funcion?
+
+    return doctors;
 }
